cf3a5fa0044537d4e74bd8ff7af21c7c9ee83070
Bikash <bikash11roy@gmail.com>
2017-03-15 00:39:42 +0600

    Pointer in go

--- Go/src/Variable/Package-level-variable.go
+++ Go/src/Variable/Package-level-variable.go
@@ -5,16 +5,20 @@ import(
 	"reflect"
 )
 
-/*
 var (
 	name string
 	course string
 	module float64
 )
-*/
+
 
 func main() {
+	// Declare and initialize variable with :=
 	name := "Bikash"
 	fmt.Println("The name is : ", name)
 	fmt.Println("The name type is : ", reflect.TypeOf(name))
+
+	// Pointer
+	fmt.Println("The address of variable name is : ", &name)
+
 }
