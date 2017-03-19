e388f9313a048746398c4f8826cb87f2d6da965e
Bikash <bikash11roy@gmail.com>
2017-03-19 13:20:51 +0600

    range in for

--- Go/src/loop/for_the_only_loop.go
+++ Go/src/loop/for_the_only_loop.go
@@ -9,6 +9,16 @@ import (
 // if no expression then it is an infinite loop
 
 func main() {
+	name := []string{
+		"Shuvendu",
+		"Roy",
+		"Bikash",
+	}
+
+	for _, i := range name{
+		fmt.Println(i)
+	}
+
 	for timer := 10; timer>=0; timer--{
 		fmt.Println(timer)
 		time.Sleep(1*time.Second)
