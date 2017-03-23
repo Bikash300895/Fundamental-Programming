c22252ee10f1c5fe82a9f4212c3971404a26fb59
Bikash <bikash11roy@gmail.com>
2017-03-23 20:29:17 +0600

    Handling web page request

--- Go/GoWeb/src/main/main.go
+++ Go/GoWeb/src/main/main.go
@@ -6,23 +6,22 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", new(MyHandler))
-
-	http.ListenAndServe(":8000", nil)
+	http.Handle("/", new(MyHandler))
+	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
 }
 
 type MyHandler struct {
 	http.Handler
 }
 
-func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request){
-	path := "public/"+req.URL.Path
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := "public" + req.URL.Path
 	data, err := ioutil.ReadFile(string(path))
 
-	if err==nil{
+	if err == nil {
 		w.Write(data)
 	} else {
 		w.WriteHeader(404)
-		w.Write([]byte("404 - "+http.StatusText(404)))
+		w.Write([]byte("404 - " + http.StatusText(404)))
 	}
 }
