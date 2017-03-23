c95e8b8cda5453d89fd43348eb9699a4d4d080ff
Bikash <bikash11roy@gmail.com>
2017-03-23 20:13:37 +0600

    Web request handeller

--- Go/GoWeb/src/main/main.go
+++ Go/GoWeb/src/main/main.go
@@ -1,11 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"io/ioutil"
+)
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
+	http.HandleFunc("/", new(MyHandler))
 
 	http.ListenAndServe(":8000", nil)
+}
+
+type MyHandler struct {
+	http.Handler
+}
+
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request){
+	path := "public/"+req.URL.Path
+	data, err := ioutil.ReadFile(string(path))
+
+	if err==nil{
+		w.Write(data)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("404 - "+http.StatusText(404)))
+	}
 }
