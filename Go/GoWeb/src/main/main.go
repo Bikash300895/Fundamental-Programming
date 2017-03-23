95a44ac04d6bec919ea0a4be4cb349bfd076e9c1
Bikash <bikash11roy@gmail.com>
2017-03-23 20:53:43 +0600

    Handling request with raw html code

--- Go/GoWeb/src/main/main.go
+++ Go/GoWeb/src/main/main.go
@@ -2,8 +2,52 @@ package main
 
 import (
 	"net/http"
+	"html/template"
 )
 
 func main() {
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+	//http.Handle("/", new(MyHandler))
+
+	//Reading data from file server
+	//http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content Type", "text/html")
+		tmpl, err := template.New("test").Parse(doc)
+
+		if err == nil{
+			tmpl.Execute(w, nil)
+		}
+	})
+	http.ListenAndServe(":8000", nil)
 }
+
+const doc = `
+<!DOCTYPE html>
+<html>
+	<head><title>Go</title></head>
+	<body>
+		<h1>Hi there</h1>
+	</body>
+</html>
+`
+
+// Handeling request menually
+/*
+type MyHandler struct {
+	http.Handler
+}
+
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := "public" + req.URL.Path
+	data, err := ioutil.ReadFile(string(path))
+
+	if err == nil {
+		w.Write(data)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("404 - " + http.StatusText(404)))
+	}
+
+}
+*/
+
