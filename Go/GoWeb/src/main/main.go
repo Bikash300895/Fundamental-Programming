72cd23a291d1e726d43d52cb7b417cfcdf5e161c
Bikash <bikash11roy@gmail.com>
2017-03-23 20:40:32 +0600

    Removing unnecessary code

--- Go/GoWeb/src/main/main.go
+++ Go/GoWeb/src/main/main.go
@@ -2,26 +2,8 @@ package main
 
 import (
 	"net/http"
-	"io/ioutil"
 )
 
 func main() {
-	http.Handle("/", new(MyHandler))
 	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
-}
-
-type MyHandler struct {
-	http.Handler
-}
-
-func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	path := "public" + req.URL.Path
-	data, err := ioutil.ReadFile(string(path))
-
-	if err == nil {
-		w.Write(data)
-	} else {
-		w.WriteHeader(404)
-		w.Write([]byte("404 - " + http.StatusText(404)))
-	}
 }
