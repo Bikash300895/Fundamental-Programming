e97dbb52ae31685b331586aa1711f6530f9cc6ca
Bikash <bikash11roy@gmail.com>
2017-03-27 02:00:25 +0600

    js compilation

---
+++ rxjs/rxjs5/main.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var rxjs_1 = require("rxjs");
+var numbers = [1, 2, 3];
+var source = rxjs_1.Observable.from(numbers);
+var MyOvservable = (function () {
+    function MyOvservable() {
+    }
+    MyOvservable.prototype.next = function (value) {
+        console.log("value : " + value);
+    };
+    MyOvservable.prototype.complete = function () {
+        console.log("complete");
+    };
+    return MyOvservable;
+}());
+source.subscribe(new MyOvservable());
+//# sourceMappingURL=main.js.map
