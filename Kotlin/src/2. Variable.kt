97683da561907192f232240fe0b2e40959198c25
Bikash <bikash11roy@gmail.com>
2017-05-23 00:25:07 +0600

    var and val

--- Kotlin/src/2. Variable.kt
+++ Kotlin/src/2. Variable.kt
@@ -1,5 +1,10 @@
 fun main(args: Array<String>){
     var a = 10
+    var name: String = "Bikash"
 
+    // Constant values in kotlin
+    val x = 10
+
+    println("Hello "+name)
     println("Value of a is ${a}")
 }
