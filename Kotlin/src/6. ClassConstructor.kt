0e05b05c5be3900d50bf388c1e22fede16ad19ce
Bikash <bikash11roy@gmail.com>
2017-05-28 17:11:22 +0600

    Secondary constructor and primary constructor at the same time

--- Kotlin/src/6. ClassConstructor.kt
+++ Kotlin/src/6. ClassConstructor.kt
@@ -1,16 +1,17 @@
-class User{
+class User(email: String){
     init {
         println("Init block executed")
 
     }
 
-    constructor(id: String, name: String, password: String){
+    constructor(id: String, name: String, email: String, password: String): this(email){
         println(id)
         println(name)
+        println(email)
         println(password)
     }
 }
 
 fun main(args: Array<String>){
-    var bikash = User("1407001", "Bikash", "pass")
+    var bikash = User("1407001", "Bikash", "bikash@gmail.com","pass")
 }
