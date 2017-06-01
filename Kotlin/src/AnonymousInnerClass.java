60d34faf40a370ef839fc8e90afb162c6e107beb
Bikash <bikash11roy@gmail.com>
2017-06-01 19:06:26 +0600

    Anonymous inner class in java duplicate solved

--- Kotlin/src/AnonymousInnerClass.java
+++ Kotlin/src/AnonymousInnerClass.java
@@ -1,11 +1,11 @@
-interface Human
+interface Human2
 {
     void think();
 }
 
 public class AnonymousInnerClass {
     public static void main(String[] args){
-        Human programmer = new Human() {
+        Human2 programmer = new Human2() {
             @Override
             public void think() {
                 System.out.print("Think virtual");
