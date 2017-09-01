5ccd44185eb4542a343344185c63caecd956f17c
Bikash <bikash11roy@gmail.com>
2017-09-01 21:10:44 +0600

    changing the look and feel
    Signed-off-by: Bikash <bikash11roy@gmail.com>

--- Matlab/PlotExample.m
+++ Matlab/PlotExample.m
@@ -8,4 +8,10 @@ x2 = linspace(-10, 10, 64);
 y2 = x2.*sin(x2);
 
 plot(x1, y1, 'linewidth',2), hold on;
-plot(x2, y2, 'k');
+plot(x2, y2, 'm:d');
+
+xlabel('x label');
+ylabel('y label');
+title('My graph','fontsize',15);
+
+legend('data 1', 'data 2', 'location', 'best');
