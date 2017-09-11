723c86f81f0c7e40f36659295be3c8985b5f03c4
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-12 03:37:06 +0600

    list
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- Swift/2. List.playground/Contents.swift
+++ Swift/2. List.playground/Contents.swift
@@ -8,6 +8,17 @@ print(list)
 
 // Dictionary
 var dict = [
-    "name" : "bikas",
+    "name" : "bikash",
     "roll" : 1407001,
 ] as [String: Any] // Note the requirement for this explicit type decleration
+
+print(dict["name"]!) // note the ! which force it to must have a value
+
+// Creating empty list
+let emptyList = [String]()
+
+// Creating empty dictionary
+let emptyDict = [String: String]()
+
+
+
