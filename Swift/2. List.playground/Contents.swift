26ffa5c96ff4877cb205befe939871086dd3fe09
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-12 03:30:39 +0600

    dictionary
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

---
+++ Swift/2. List.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var list = [1, 2, 3, 4, 5]
+print(list[2])
+
+print(list)
+
+
+// Dictionary
+var dict = [
+    "name" : "bikas",
+    "roll" : 1407001,
+] as [String: Any] // Note the requirement for this explicit type decleration
