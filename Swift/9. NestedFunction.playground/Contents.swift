2078c8b404f47b9d5c0ce1af2c6619017c3def31
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-12 04:43:43 +0600

    function as argument
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- Swift/9. NestedFunction.playground/Contents.swift
+++ Swift/9. NestedFunction.playground/Contents.swift
@@ -27,4 +27,21 @@ print(increment(7))
 
 
 // Function can take other function as its argument
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item){
+            return true
+        }
+    }
+    return false
+}
+
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+
+var numbers = [20, 19, 7, 15]
+print(hasAnyMatches(list: numbers, condition: lessThanTen))
+
+
 
