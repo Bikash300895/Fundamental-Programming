946962176227b8b6271e5c18812b9343a59a230c
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-12 03:25:58 +0600

    basics
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- Swift/1. Variables.playground/Contents.swift
+++ Swift/1. Variables.playground/Contents.swift
@@ -14,4 +14,20 @@ print(myVar)
 let constantVar = 18
 print(constantVar)
 
+// Explicit data type
+let explicitVar: Double
+explicitVar = 25
+print(explicitVar)
 
+// String and explicit conversion
+let label = "The width is "
+let width = 10.5
+print(label + String(width))
+
+print("The width is \(width + 1)")
+
+"""
+This is the way to
+make long comment
+the cote must go and start with new line
+"""
