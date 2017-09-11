58549b07d1bf86e45e737001e8db472c2725833c
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-12 04:55:43 +0600

    inheriatance
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- Swift/10. Class.playground/Contents.swift
+++ Swift/10. Class.playground/Contents.swift
@@ -1,11 +1,42 @@
 class Shape {
     var numberOfSides = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
     
     func simpleSecription() -> String {
         return "A shape with \(numberOfSides) sides"
     }
 }
 
-var shape = Shape()
+var shape = Shape(name: "rectangle")
 shape.numberOfSides = 3
 print(shape.simpleSecription())
+
+// Inheritance
+class Square: Shape {
+    var sideLength: Double
+    
+    init(sideLength: Double, name:String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        super.numberOfSides = 4
+        // or just    numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleSecription() -> String {
+        return "A Square with sides of length \(sideLength)"
+    }
+}
+
+let sqr = Square(sideLength: 2.3, name: "test square")
+print(sqr.area())
+print(sqr.simpleSecription())
+
+
